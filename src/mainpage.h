18802f4499ca0695402802bbb8324206ef707f68
heather <>
2004-07-13 22:17:15 +0000

    initial import of the new rootTestData package

---
+++ src/mainpage.h
@@ -0,0 +1,20 @@
+/** @mainpage package rootTestData 
+
+  @section intro Introduction
+  This package is a central location for ROOT test data. 
+  
+  <hr>
+  @section data data
+  The data directory contains the following directories:
+  default - Contains the standard test data copied from the AnalysisNtuple package
+
+  <hr>
+  @section notes release notes
+  release.notes
+  <hr>
+  @section requirements requirements
+  @verbinclude requirements
+
+  @authors Heather Kelly
+*/
+
